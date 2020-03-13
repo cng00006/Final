813569f3ecbc58ad8f8cf0d2fc1a03513bc50608
cng00006 <55862984+cng00006@users.noreply.github.com>
2020-03-12 19:54:47 -0700

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server2" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+
 yum install -y nfs-utils
 mkdir /var/nfsshare
 mkdir /var/nfsshare/devstuff
