8161e298b0c1b8032276975759b97a87758a466d
cng00006 <55862984+cng00006@users.noreply.github.com>
2020-03-18 18:02:51 -0700

    Update nfs.sh

--- nfs.sh
+++ nfs.sh
@@ -2,6 +2,32 @@
 
 echo "*.info;mail.none;authpriv.none;cron.none   @rsyslog-server2" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
 
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+
+
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://34.71.91.10/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://34.71.91.10/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://34.71.91.10/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://34.71.91.10/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
 yum install -y nfs-utils
 mkdir /var/nfsshare
 mkdir /var/nfsshare/devstuff
