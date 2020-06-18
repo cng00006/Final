135ba3a82b64e19b48b20a1f415b43e568905402
cng00006 <55862984+cng00006@users.noreply.github.com>
2020-06-18 10:19:02 -0700

    Update and rename final.sh to final320.sh

--- final320.sh
+++ final320.sh
@@ -1,4 +1,31 @@
 #!bin/bash
+gcloud compute instances create build \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central1-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/cng00006/Final/build_enviroment.sh \
+--private-network-ip=10.128.0.42
+gcloud compute instances create nagios \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central1-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/cng00006/Final/nagios.sh \
+--private-network-ip=10.128.0.41
+gcloud compute instances create cacti \
+--image-family centos-7 \
+--image-project centos-cloud \
+--zone us-central1-a \
+--tags "http-server","https-server" \
+--machine-type f1-micro \
+--scopes cloud-platform \
+--metadata-from-file startup-script=/home/cng00006/Final/cacti_server.sh \
+--private-network-ip=10.128.0.40
 gcloud compute instances create rsyslog-server2 \
 --image-family centos-7 \
 --image-project centos-cloud \
