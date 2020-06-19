63b85034dae2636ba3b0dce55112b7440ce1b855
cng00006 <55862984+cng00006@users.noreply.github.com>
2020-06-18 18:09:46 -0700

    Create build_server.sh

---
+++ build_server.sh
@@ -0,0 +1,13 @@
+  
+#!/bin/bash
+# This should be the finishing script for a micro with a 50G hard drive
+
+yum -y install rpm-build make gcc git                                         # install rpm tools, compiling tools and source tools
+mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}                      # create the rpmbuild dirrectory structure
+                                                                              # (the docs say this happens by default, this is incorrect on centos 7)
+cd ~/
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros                         # Set the rpmbuild path in an .rpmmacros file
+cd ~/rpmbuild/SOURCES
+
+cd ../SPECS                                                                   # head to the SPECS directory
+cp /usr/share/vim/vimfiles/template.spec . 
