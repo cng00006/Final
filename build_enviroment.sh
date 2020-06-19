eb0f43b7c08b8efe7edac44485d0d3a11759f899
cng00006 <55862984+cng00006@users.noreply.github.com>
2020-06-18 18:55:22 -0700

    Update build_enviroment.sh

--- build_enviroment.sh
+++ build_enviroment.sh
@@ -1,6 +1,27 @@
   
 #!/bin/bash
 # This should be the finishing script for a micro with a 50G hard drive
+for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done
+echo "[nti-310-epel]
+name=NTI310 EPEL
+baseurl=http://104.197.59.12/epel
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-base]
+name=NTI310 BASE
+baseurl=http://104.197.59.12/base
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-extras]
+name=NTI310 EXTRAS
+baseurl=http://104.197.59.12/extras/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
+echo "[nti-310-updates]
+name=NTI310 UPDATES
+baseurl=http://104.197.59.12/updates/
+gpgcheck=0
+enabled=1" >> /etc/yum.repos.d/local-repo.repo
 
 yum -y install rpm-build make gcc git                                         # install rpm tools, compiling tools and source tools
 mkdir -p /root/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}                      # create the rpmbuild dirrectory structure
